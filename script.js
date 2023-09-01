4dcf3afa8ff380d805fa7f06265b768dcbcb9788
Have Samuel <samiehave9@gmail.com>
2023-09-01 11:40:12 +0300

    Remove unwanted code

--- script.js
+++ script.js
@@ -14,38 +14,12 @@
 // });
 
 // DropDown Icon
-// const links = document.querySelectorAll('.nav-item');
+const links = document.querySelectorAll('.nav-item');
 
-// links.forEach((link) => {
-//   link.addEventListener('click', () => {
-//     link.classList.value.includes('active');
-//     link.classList.toggle('active');
-//     console.log(link);
-//   });
-// });
-
-// const link = document.querySelector('.nav-item');
-
-// link.addEventListener('click', () => {
-//   link.classList.toggle('active');
-//   console.log(link);
-// });
-
-function myFunction() {
-  document.getElementById('').classList.toggle('active');
-}
-
-myFunction();
-
-window.onclick = ((event) => {
-  if (!event.target.matches('.nav-link')) {
-    const dropdowns = document.getElementsByClassName('dropdown-container');
-    let i;
-    for (i = 0; i < dropdowns.length; i + 1) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('active')) {
-        openDropdown.classList.remove('active');
-      }
-    }
-  }
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    link.classList.value.includes('active');
+    link.classList.toggle('active');
+    console.log(link);
+  });
 });
