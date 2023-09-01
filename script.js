76fe7d8571b2e4919213b2302378f5de19bf0647
Have Samuel <samiehave9@gmail.com>
2023-09-01 10:13:09 +0300

    Commenting and checks

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const menuIcon = document.querySelector('.close-icon');
-const navbar = document.querySelector('.navbar');
+// const menuIcon = document.querySelector('.close-icon');
+// const navbar = document.querySelector('.navbar');
 
-const list = document.querySelectorAll('nav-list > li');
-list.forEach((ele) => {
-  ele.addEventListener('click', () => {
-    navbar.classList.toggle('change');
-    console.log(list);
-  });
-});
+// const list = document.querySelectorAll('nav-list > li');
+// list.forEach((ele) => {
+//   ele.addEventListener('click', () => {
+//     navbar.classList.toggle('change');
+//     console.log(list);
+//   });
+// });
 
 // menuIcon.addEventListener('click', () => {
 //   navbar.classList.toggle('change');
