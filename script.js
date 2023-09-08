5eab095cf7b0402702ecebe86b214cd94bfa461d
Have Samuel <samiehave9@gmail.com>
2023-09-08 22:47:45 +0300

    Remove the Comments

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// const menuIcon = document.querySelector('.close-icon');
-// const navbar = document.querySelector('.navbar');
 
-// const list = document.querySelectorAll('nav-list > li');
-// list.forEach((ele) => {
-//   ele.addEventListener('click', () => {
-//     navbar.classList.toggle('change');
-//     console.log(list);
-//   });
-// });
-
-// menuIcon.addEventListener('click', () => {
-//   navbar.classList.toggle('change');
-// });
 
 // DropDown Icon
 function myFunction() {
