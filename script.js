46a64f64cbaddfee74715b73fc8a84328de36892
Have Samuel <samiehave9@gmail.com>
2023-09-17 11:12:48 +0300

    Creating the dropdown

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Clickable arrow to make the dropdown menu appear
 const dropUp = document.querySelector('.drop-up');
+
+dropUp.addEventListener('click', () => {
+  const dropDown = document.querySelector('.arrow-down');
+  dropDown.classList.toggle('active');
+  dropUp.classList.toggle('active');
+});
