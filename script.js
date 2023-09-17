533a1695d944cc63cd0a5247189d56828ccfe1a1
Have Samuel <samiehave9@gmail.com>
2023-09-17 11:46:47 +0300

    Adding an eventListener

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Clickable arrow to make the dropdown menu appear
 const dropUp = document.querySelector('#drop-up');
 
-
+// When you click the arrow
+dropUp.addEventListener('click', () => {
+ 
+});
