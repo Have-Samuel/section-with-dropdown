7aaa3b9efd6a2d5b2424ad8023ca5de8a892ef8c
Have Samuel <samiehave9@gmail.com>
2023-09-23 23:21:07 +0300

    Close the dropdown menu

--- script.js
+++ script.js
@@ -9,3 +9,17 @@
 function myFunction() {
   document.getElementById('myDropdown').classList.toggle('active');
 }
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-up')) {
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    let i;
+    for (i = 0; i < dropdowns.length; i + 1) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('active')) {
+        openDropdown.classList.remove('active');
+      }
+    }
+  }
+};
