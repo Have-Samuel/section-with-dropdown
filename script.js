dc7fdb78c741c9a3b28789cd0a5314af87a5869f
Have Samuel <samiehave9@gmail.com>
2023-09-25 12:51:16 +0300

    Remove the unwanted code

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 // Clickable arrow to make the dropdown menu appear
-// const dropUp = document.querySelector('#drop-up');
 
-// // When you click the arrow
-// dropUp.addEventListener('clcik', () => {
-  
-// })
 
-// function myFunction() {
-//   document.getElementById('myDropdown').classList.toggle('active');
-// }
-
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function (event) {
-//  if (!event.target.matches('.dropbtn')) {
-//    var dropdowns = document.getElementsByClassName('dropdown-content');
-//    var i;
-//    for (i = 0; i < dropdowns.length; i++) {
-//      var openDropdown = dropdowns[i];
-//      if (openDropdown.classList.contains('active')) {
-//        openDropdown.classList.remove('active');
-//      }
-//    }
-//  }
-// };
-
+// Hamburger menu, when you click on it, the menu appears
 const humburger = document.querySelector('.humburger');
 const navbar = document.querySelector('.navbar');
 
